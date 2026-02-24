13922cfa2d936f89e44f7fb37c0054e28d831368
LucasCrippa <lucas_crippa@hotmail.com>
2026-02-24 09:13:43 -0300

    Subindo o projeto no github

---
+++ src/test/java/br/com/lucascrippa/ordemservico/OrdemServicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucascrippa.ordemservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemServicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
